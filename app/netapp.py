1a3469bc7b52c9656c30b499c9a45653324ddbab
caleb <piepieninja@gmail.com>
2016-02-14 02:44:06 +0000

    fullscreen gui

--- app/netapp.py
+++ app/netapp.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
+##
+# Author: Caleb Adams
+# Discription: Uses the tkinter 
+#
+#
 
 import Tkinter
-top = Tkinter.Tk()
-# Code to add widgets will go here...
-top.mainloop()
+
+
+root = Tkinter.Tk()
+
+# make it cover the entire screen
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+root.overrideredirect(1)
+root.geometry("%dx%d+0+0" % (w, h))
+
+root.focus_set() # <-- move focus to this widget
+root.bind("<Escape>", lambda e: e.widget.quit())
+
+root.mainloop()
