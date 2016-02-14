ad09175043fcda19852e259504c444ec4d2f3fdb
caleb <piepieninja@gmail.com>
2016-02-14 03:02:27 +0000

    example button. backround change

--- app/netapp.py
+++ app/netapp.py
@@ -12,10 +12,14 @@ root = Tkinter.Tk()
 
 # make it cover the entire screen
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-root.overrideredirect(1)
+#root.overrideredirect(1) # <-- this removes the menu items on top. commented for testing
 root.geometry("%dx%d+0+0" % (w, h))
-
+root.configure(background='black')
 root.focus_set() # <-- move focus to this widget
 root.bind("<Escape>", lambda e: e.widget.quit())
 
+B = Tkinter.Button(root, text ="test", bg ="grey")
+
+
+B.pack()
 root.mainloop()
