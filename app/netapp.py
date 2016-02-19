4e9901ea8886a29214f2245ab8c92577d6643845
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-02-19 00:16:32 -0500

    removes image and PIL

--- app/netapp.py
+++ app/netapp.py
@@ -6,14 +6,14 @@
 #
 
 import Tkinter
-import PIL
+#import PIL
 
 root = Tkinter.Tk()
 
 # make it cover the entire screen
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 #root.overrideredirect(1) # <-- this removes the menu items on top. commented for testing
-window.title("Hacker House Network")
+root.title("Hacker House Network")
 root.geometry("%dx%d+0+0" % (w, h))
 root.configure(background='black')
 root.focus_set() # <-- move focus to this widget
@@ -21,13 +21,15 @@ root.bind("<Escape>", lambda e: e.widget.quit())
 
 B1 = Tkinter.Button(root, text="test", bg="grey")
 B2 = Tkinter.Button(root, text="test", bg="grey")
+B3 = Tkinter.Button(root, text="test", bg="grey")
 
-HAXimg = ImageTk.PhotoImage(Image.open("img/HAX.png"))
-HAXlabel = Label(root, image=img)
+#HAXimg = ImageTk.PhotoImage(Image.open("img/HAX.png"))
+#HAXlabel = Label(root, image=img)
 
 B1.grid(row=2, column=0)
 B2.grid(row=2, column=1)
+B3.grid(row=2, column=2)
 
-HAXlabel.grid(row=0, column=3)
+#HAXlabel.grid(row=0, column=3)
 
 root.mainloop()
