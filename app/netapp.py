8514bdbc76d5066dcf9506e5ed050800d6e8e6cd
Caleb <piepieninja@gmail.com>
2016-02-19 06:41:59 +0000

    basic labels

--- app/netapp.py
+++ app/netapp.py
@@ -23,6 +23,12 @@ root.configure(background='black')
 root.focus_set() # <-- move focus to this widget
 root.bind("<Escape>", lambda e: e.widget.quit())
 
+label1 = Label(root, text="Hello, world!")
+label2 = Label(root, text="Hello, world!")
+label3 = Label(root, text="Hello, world!")
+label4 = Label(root, text="Hello, world!")
+label5 = Label(root, text="")
+
 B1 = Tkinter.Button(root, text="test", bg="grey")
 B2 = Tkinter.Button(root, text="test", bg="grey")
 B3 = Tkinter.Button(root, text="test", bg="grey")
@@ -30,10 +36,16 @@ B3 = Tkinter.Button(root, text="test", bg="grey")
 HAXimgtk = ImageTk.PhotoImage(Image.open("img/HAX.png"))
 HAXlabel = Label(root, image=HAXimgtk)
 
-B1.grid(row=2, column=0)
-B2.grid(row=2, column=1)
-B3.grid(row=2, column=2)
+label1.grid(row=0, column=0)
+label2.grid(row=1, column=0)
+label3.grid(row=2, column=0)
+label4.grid(row=3, column=0)
+label5.grid(row=4, column=0)
 
 HAXlabel.grid(row=0, column=3)
+
+B1.grid(row=5, column=0)
+B2.grid(row=5, column=1)
+B3.grid(row=5, column=2)
 
 root.mainloop()
