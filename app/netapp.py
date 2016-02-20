dc3c3868bb542126499132278a8b07927a2984e8
Caleb <piepieninja@gmail.com>
2016-02-20 04:14:27 +0000

    stable speed tests

--- app/netapp.py
+++ app/netapp.py
@@ -17,7 +17,7 @@ from PIL import Image, ImageTk
 
 #### GLOBALS ####
 OS = ""
-debug = TRUE
+debug = True
 
 ################# SYS ################# 
 
@@ -28,15 +28,10 @@ def getOS(): #it's probably better to just do this once
 
 def tick():
     global time1
-    # get the current local time from the PC
     time2 = time.strftime('%H:%M:%S')
-    # if time string has changed, update it
     if time2 != time1:
         time1 = time2
         label5.config(text=time2)
-    # calls itself every 200 milliseconds
-    # to update the time display as needed
-    # could use >200 ms, but display gets jerky
     label5.after(200, tick)
 
 ################# NET ################# 
@@ -87,15 +82,34 @@ def downSpeed():
 
 #### LOOPS ####
 
+def upTick():
+    if (debug):
+        print "testing up-speed..."
+    global up_speed
+    temp = upSpeed()
+    up_speed = temp
+    label3.config(text=temp)
+    label3.after(7200000, upTick) # every 2 hours
+
+def downTick():
+    if (debug):
+        print "testing down-speed..."
+    global down_speed
+    temp = downSpeed()
+    down_speed = temp
+    label4.config(text=temp)
+    label4.after(7200000, downTick) # every 2 hours
+
 ################# GUI #################
 
 root = Tkinter.Tk()
 
 # make it cover the entire screen
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-#root.overrideredirect(1) # <-- this removes the menu items on top. commented for testing
 root.title("Hacker House Network")
-#root.geometry("%dx%d+0+0" % (w, h))
+if (not debug):
+    root.overrideredirect(1) # <-- this removes the menu items on top. commented for testing
+    root.geometry("%dx%d+0+0" % (w, h))
 root.configure(background='black')
 root.focus_set() # <-- move focus to this widget
 root.bind("<Escape>", lambda e: e.widget.quit())
@@ -118,17 +132,17 @@ my_ip = grabIP()
 label1 = Label(dataFrame, anchor=N, text = "IP: " + my_ip, font="-weight bold", fg="green", bg="black")
 devices = onlineDevices()
 label2 = Label(dataFrame, anchor=N, text = "Devices: " + devices, font="-weight bold", fg="green", bg="black")
-up_speed = upSpeed()
+up_speed = 'Upload: testing...' #upSpeed()
 label3 = Label(dataFrame, anchor=N, text = up_speed, font="-weight bold", fg="green", bg="black")
-down_speed = downSpeed()
+down_speed = "Download: testing..." #downSpeed()
 label4 = Label(dataFrame, anchor=N, text = down_speed, font="-weight bold", fg="green", bg="black")
 time1 = ''
 label5 = Label(dataFrame, anchor=N, font="-weight bold", fg="green", bg="black")
 
 
-B1 = Tkinter.Button(navFrame, text="TBD", bg="grey")
-B2 = Tkinter.Button(navFrame, text="TBD", bg="grey")
-B3 = Tkinter.Button(navFrame, text="TBD", bg="grey")
+#B1 = Tkinter.Button(navFrame, text="TBD", bg="grey")
+#B2 = Tkinter.Button(navFrame, text="TBD", bg="grey")
+#B3 = Tkinter.Button(navFrame, text="TBD", bg="grey")
 
 HAXimgtk = ImageTk.PhotoImage(Image.open("img/HAX.png"))
 HAXlabel = Label(imgFrame, image=HAXimgtk)
@@ -142,9 +156,11 @@ label5.grid(row=4, column=0, sticky=Tkinter.W)
 
 HAXlabel.grid(row=0, column=0, sticky=Tkinter.E);
 
-B1.grid(row=5, column=0, sticky=Tkinter.E)
-B2.grid(row=5, column=1, sticky=Tkinter.E)
-B3.grid(row=5, column=2, sticky=Tkinter.E)
+#B1.grid(row=5, column=0, sticky=Tkinter.E)
+#B2.grid(row=5, column=1, sticky=Tkinter.E)
+#B3.grid(row=5, column=2, sticky=Tkinter.E)
 
 tick()
+upTick()
+downTick()
 root.mainloop()
