68f8e875437153cf8533daa4abc190c6551af14f
Caleb <piepieninja@gmail.com>
2016-02-20 01:37:51 +0000

    better frames

--- app/netapp.py
+++ app/netapp.py
@@ -19,6 +19,9 @@ def grabIP():
     my_ip = s.getsockname()[0]
     return my_ip;
 
+def onlineDevices():
+    return "NOT IMPLEMENTED";
+
 ################# GUI #################
 
 root = Tkinter.Tk()
@@ -27,22 +30,26 @@ root = Tkinter.Tk()
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 #root.overrideredirect(1) # <-- this removes the menu items on top. commented for testing
 root.title("Hacker House Network")
-root.geometry("%dx%d+0+0" % (w, h))
+#root.geometry("%dx%d+0+0" % (w, h))
 root.configure(background='black')
 root.focus_set() # <-- move focus to this widget
 root.bind("<Escape>", lambda e: e.widget.quit())
 
 # frames
-dataFrame = Frame(root, bg="black")
+topHolder = Frame(root, bg="black")
+topHolder.pack(side=TOP)
+dataFrame = Frame(topHolder, bg="black")
 dataFrame.pack(side=LEFT)
-imgFrame = Frame(root, bg="black")
+imgFrame = Frame(topHolder, bg="black")
 imgFrame.pack(side=RIGHT)
 navFrame = Frame(root, bg="black")
 navFrame.pack(side=BOTTOM, fill=X)
 
 # widgets
 my_ip = grabIP()
-label1 = Label(dataFrame, text = "IP: " + my_ip, font="-weight bold", fg="green", bg="black")
+label1 = Label(dataFrame, anchor=N, text = "IP: " + my_ip, font="-weight bold", fg="green", bg="black")
+devices = onlineDevices()
+label2 = Label(dataFrame, anchor=N, text = "Devices: " + devices, font="-weight bold", fg="green", bg="black")
 
 B1 = Tkinter.Button(navFrame, text="test", bg="grey")
 B2 = Tkinter.Button(navFrame, text="test", bg="grey")
@@ -52,11 +59,10 @@ HAXimgtk = ImageTk.PhotoImage(Image.open("img/HAX.png"))
 HAXlabel = Label(imgFrame, image=HAXimgtk)
 
 # grid assignments
-#label1.grid(row=0, column=0)
-label1.pack()
+label1.grid(row=0, column=0)
+label2.grid(row=1, column=0)
 
-#HAXlabel.grid(row=0, column=3)
-HAXlabel.pack();
+HAXlabel.pack(anchor=E);
 
 B1.grid(row=5, column=0)
 B2.grid(row=5, column=1)
